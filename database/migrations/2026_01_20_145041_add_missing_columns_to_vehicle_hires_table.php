42a404edf5c7395ac38c2ad39d4f49b71253bed9
Anchal <svpinfotech11@gmail.com>
2026-01-20 15:27:31 +0530

    ....

---
+++ database/migrations/2026_01_20_145041_add_missing_columns_to_vehicle_hires_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicle_hires', function (Blueprint $table) {
+            $table->foreignId('vehicle_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('driver_id')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicle_hires', function (Blueprint $table) {
+            $table->dropForeign(['vehicle_id']);
+            $table->dropForeign(['driver_id']);
+            $table->dropColumn(['vehicle_id', 'driver_id']);
+        });
+    }
+};
